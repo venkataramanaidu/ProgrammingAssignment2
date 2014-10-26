8095a91f303c7da9d4ab5fa4fa1eebd83e3f0ff5
venkataramanaidu <ramanaidu1956@gmail.com>
2014-10-26 09:58:03 -0400

    This is a set of two functions. One to get/set and inverse a matrix and the other function to either get a cached inverse of a matrix if the matrix did not change. If changed , will return the inversed matrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
+## this function gets and sets the matrices and generates the inverse of a matrix using the solve function
+
 makeCacheMatrix <- function(x = matrix()) {
+
+       m <- x
+        set <- function(y= matrix()) {
+                x <<- y
+                m <<- x
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve(x)
+        getinverse <- function() solve(m)
+        list(set = set, get = get,
+             setinverse  = setinverse ,
+             getinverse = getinverse )
 
 }
 
-
 ## Write a short comment describing this function
+
+## This function gets the cashed inveresd matrix if the matrix did not change otherwise computes and returns the inverse
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$getinverse(m)
+        m
 }
